9ceac5d4208021b99934c2317673676e03ec4a9b
phamthao <phamthao03596@gmail.com>
2021-05-09 11:07:22 +0700

    feat: add support laravel resource

---
+++ src/HasCursorPaginate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Vanthao03596\LaravelCursorPaginate;
+
+use Illuminate\Http\Resources\MissingValue;
+use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Support\Collection;
+
+trait HasCursorPaginate
+{
+    /**
+     * Map the given collection resource into its individual resources.
+     *
+     * @param  mixed  $resource
+     * @return mixed
+     */
+    protected function collectResource($resource)
+    {
+        if ($resource instanceof MissingValue) {
+            return $resource;
+        }
+
+        if (is_array($resource)) {
+            $resource = new Collection($resource);
+        }
+
+        $collects = $this->collects();
+
+        $this->collection = $collects && ! $resource->first() instanceof $collects
+            ? $resource->mapInto($collects)
+            : $resource->toBase();
+
+        return ($resource instanceof AbstractPaginator || $resource instanceof AbstractCursorPaginator)
+            ? $resource->setCollection($this->collection)
+            : $this->collection;
+    }
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        if ($this->resource instanceof AbstractPaginator || $this->resource instanceof AbstractCursorPaginator) {
+            return $this->preparePaginatedResponse($request);
+        }
+
+        return parent::toResponse($request);
+    }
+}
