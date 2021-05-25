b40988a517eb90e094df9114a67972960f6e6885
phamthao <phamthao03596@gmail.com>
2021-05-25 08:14:22 +0700

    chore: Update Cursor.php

--- src/Cursor.php
+++ src/Cursor.php
@@ -41,7 +41,7 @@ class Cursor implements Arrayable
      */
     public function parameter(string $parameterName)
     {
-        if (! isset($this->parameters[$parameterName])) {
+        if (! array_key_exists($parameterName, $this->parameters)) {
             throw new UnexpectedValueException("Unable to find parameter [{$parameterName}] in pagination item.");
         }
 
