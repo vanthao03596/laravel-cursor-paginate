8b6afa69d6e90d5af25ba8da7a06d0a02e56847b
phamthao <phamthao03596@gmail.com>
2021-08-03 10:55:43 +0700

    Fix previous column for cursor pagination

--- src/EloquentCursorPaginateMixin.php
+++ src/EloquentCursorPaginateMixin.php
@@ -22,7 +22,11 @@ class EloquentCursorPaginateMixin
             if (! is_null($cursor)) {
                 $addCursorConditions = function (self $builder, $previousColumn, $i) use (&$addCursorConditions, $cursor, $orders) {
                     if (! is_null($previousColumn)) {
-                        $builder->where($previousColumn, '=', $cursor->parameter($previousColumn));
+                        $builder->where(
+                            $this->getOriginalColumnNameForCursorPagination($this, $previousColumn),
+                            '=',
+                            $cursor->parameter($previousColumn)
+                        );
                     }
     
                     $builder->where(function (self $builder) use ($addCursorConditions, $cursor, $orders, $i) {
